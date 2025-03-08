573ca15bd7cd748f7aca74f47135f4af06836643
Firas-AbuMalloh <firas1995f1995@gmail.com>
2025-03-09 01:58:53 +0300

    final project mvc

---
+++ SpaceSoftSolutions/Models/SpaceSoftSolutionsContext.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpaceSoftSolutions.Models;
+
+public partial class SpaceSoftSolutionsContext : DbContext
+{
+    public SpaceSoftSolutionsContext()
+    {
+    }
+
+    public SpaceSoftSolutionsContext(DbContextOptions<SpaceSoftSolutionsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Attendance> Attendances { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Evaluation> Evaluations { get; set; }
+
+    public virtual DbSet<Feedback> Feedbacks { get; set; }
+
+    public virtual DbSet<HourlyLeave> HourlyLeaves { get; set; }
+
+    public virtual DbSet<Hr> Hrs { get; set; }
+
+    public virtual DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    public virtual DbSet<Manager> Managers { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-HBCC5GE;Database=SpaceSoftSolutions;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Attendance>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Attendan__3214EC27E42E321F");
+
+            entity.ToTable("Attendance");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.PunchIn).HasColumnType("datetime");
+            entity.Property(e => e.PunchOut).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Attendances)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__Attendanc__Emplo__52593CB8");
+        });
+
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC27BF1D6A42");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC2768C2432D");
+
+            entity.HasIndex(e => e.Email, "UQ__Employee__A9D10534C284CBCE").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.GoogleId)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("GoogleID");
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Position)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__Employees__Depar__46E78A0C");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.ManagerId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__Employees__Manag__45F365D3");
+        });
+
+        modelBuilder.Entity<Evaluation>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Evaluati__3214EC27E108B5AC");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Comments)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DateEvaluated).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.ManagerId).HasColumnName("ManagerID");
+            entity.Property(e => e.Score)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Evaluations)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__Evaluatio__Emplo__5EBF139D");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.Evaluations)
+                .HasForeignKey(d => d.ManagerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Evaluatio__Manag__5FB337D6");
+        });
+
+        modelBuilder.Entity<Feedback>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Feedback__3214EC2782B20442");
+
+            entity.ToTable("Feedback");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Message).IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.SubmittedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<HourlyLeave>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__HourlyLe__3214EC279EC12AFA");
+
+            entity.ToTable("HourlyLeave");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Reason)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValue("Pending");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.HourlyLeaves)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__HourlyLea__Emplo__59FA5E80");
+        });
+
+        modelBuilder.Entity<Hr>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__HR__3214EC2733C9E6AE");
+
+            entity.ToTable("HR");
+
+            entity.HasIndex(e => e.Email, "UQ__HR__A9D1053433E75907").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.GoogleId)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("GoogleID");
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<LeaveRequest>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__LeaveReq__3214EC279B582AFD");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Reason)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValue("Pending");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.LeaveRequests)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__LeaveRequ__Emplo__5535A963");
+        });
+
+        modelBuilder.Entity<Manager>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Managers__3214EC278844E7F0");
+
+            entity.HasIndex(e => e.Email, "UQ__Managers__A9D10534325171E3").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.GoogleId)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("GoogleID");
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Managers)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__Managers__Depart__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Tasks__3214EC27F6BD774F");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValue("To Do");
+            entity.Property(e => e.TaskName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Tasks)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__Tasks__EmployeeI__4BAC3F29");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
